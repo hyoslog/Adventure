00df798cab6fe4860e2eefbf8cf4cd1d4b029ed3
hyostory <hyoslog@gmail.com>
2023-12-12 22:38:54 +0900

    프로젝트 생성
    + 프로젝트 디폴트
      - 템플릿: Third Person
      - 구현: C++
      - 타겟 플랫폼: Desktop
      - 퀄리티 프리셋: Maximum
      - 레이 트레이싱 off
      - 시작용 콘텐츠 off

---
+++ Source/Adventure/Adventure.Build.cs
@@ -0,0 +1,13 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Adventure : ModuleRules
+{
+	public Adventure(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+	}
+}
